83f329f9ff4f7f270dea28850d4145a3b5f66b4c
Rodaxfck <guillaume.delaval@outlook.fr>
2015-10-30 01:31:30 +0100

    Fix d'un bug sur les variables de session dans la partie admin + Modifications CSS pour l'affichage de produits

--- controller/admin/commentaires/control.php
+++ controller/admin/commentaires/control.php
@@ -12,25 +12,32 @@ class controleurCommentaires {
 			$this->Dir = getcwd().'/../../../';
 			require_once getcwd().'/../../../model/get.php';
 			require_once getcwd().'/../../../model/user.php';
+			require_once getcwd().'/../../../Class/panier.class.php';
 		}
 		if(file_exists(getcwd().'/../../model/model.php')) {
 			$this->Dir = getcwd().'/../../';
 			require_once getcwd().'/../../model/get.php';
 			require_once getcwd().'/../../model/user.php';
+			require_once getcwd().'/../../Class/panier.class.php';
 		}
 		if(file_exists(getcwd().'/../model/model.php')) {
 			$this->Dir = getcwd().'/../';
 			require_once getcwd().'/../model/get.php';
 			require_once getcwd().'/../model/user.php';
+			require_once getcwd().'/../Class/panier.class.php';
 		}
 		if(file_exists(getcwd().'/model/model.php')) {
 			$this->Dir = getcwd().'/';
 			require_once getcwd().'/model/get.php';
 			require_once getcwd().'/model/user.php';
+			require_once getcwd().'/Class/panier.class.php';
 		}
 
 		$this->user = new User();
 		$this->get = new Get();
+
+		// Il est obligatoire de faire un session start après l'include de la classe
+		session_start();
 
 	}
 
